aa3dbffba2ed1e759edeeffa63ff1c0fca5cffa4
Claire McGuire <claireviolet05@icloud.com>
2024-02-01 09:07:42 -0600

    Initial Commit

---
+++ MapViewNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapViewNotes
+//
+//  Created by CLAIRE MCGUIRE on 2/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
